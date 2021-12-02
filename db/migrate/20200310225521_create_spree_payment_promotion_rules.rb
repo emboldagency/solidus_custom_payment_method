84c4514e480ef9ca94a25c1672dec3def9531fbc
Given Reed <given@embold.com>
2021-12-02 15:38:22 -0500

    this time will fix the migrations... I hope

--- db/migrate/20200310225521_create_spree_payment_promotion_rules.rb
+++ db/migrate/20200310225521_create_spree_payment_promotion_rules.rb
@@ -1,8 +1,8 @@
 class CreateSpreePaymentPromotionRules < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_payment_promotion_rules do |t|
-      t.integer :payment_method_id, :integer
-      t.integer :promotion_rule_id, :integer
+      t.integer :payment_method_id
+      t.integer :promotion_rule_id
       t.timestamps
     end
   end
