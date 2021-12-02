3f8c734542b6ec569bb6007e2b0485540f4de6c4
Given Reed <given@embold.com>
2021-12-02 15:55:12 -0500

    removing relationship declaration that goes somewhere else

--- app/models/spree/payment_method/custom_cash_method.rb
+++ app/models/spree/payment_method/custom_cash_method.rb
@@ -1,7 +1,7 @@
 class Spree::PaymentMethod::CustomCashMethod < Spree::PaymentMethod::Check
   has_many :payment_promotion_rules, foreign_key: :payment_method_id
   has_many :purchase_orders, class_name: "Spree::PurchaseOrder"
-  belongs_to :orders, foreign_key: :order_id
+  # belongs_to :orders, foreign_key: :order_id
 
   def payment_source_class
     Spree::PurchaseOrder
