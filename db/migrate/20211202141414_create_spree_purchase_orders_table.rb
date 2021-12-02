fcd29d5fce42d3b4031e9c7ed651e53937c5fa75
Given Reed <given@embold.com>
2021-12-02 14:58:19 -0500

    fixed misnamed table in migration

--- db/migrate/20211202141414_create_spree_purchase_orders_table.rb
+++ db/migrate/20211202141414_create_spree_purchase_orders_table.rb
@@ -1,6 +1,6 @@
 class CreatePurchaseOrdersTable < ActiveRecord::Migration[6.1]
   def change
-    create_table :purchase_orders, force: :cascade do |t|
+    create_table :spree_purchase_orders, force: :cascade do |t|
       t.string "po_num"
       t.integer "payment_method_id"
       t.timestamps
