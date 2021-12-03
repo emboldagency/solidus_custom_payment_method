badc2d19a2a2b2d76c4716f43b169a888f2f087b
Given Reed <given@embold.com>
2021-12-03 08:45:41 -0500

    attempt at fixing the decorators, #1kj42mk

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-module Spree::PaymentDecorator
-  def self.prepended(base)
-    base.after_create :set_correct_amount
+module CustomCashMethod
+  module Spree::PaymentDecorator
+    def self.prepended(base)
+      base.after_create :set_correct_amount
+    end
+
+    private
+
+    def set_correct_amount
+      Spree::PromotionHandler::Cart.new(self.order).activate
+      self.order.recalculate
+      self.amount = self.order.total
+      self.save(validate: false)
+    end
+
+    ::Spree::Payment.prepend self
   end
-
-  private
-
-  def set_correct_amount
-    Spree::PromotionHandler::Cart.new(self.order).activate
-    self.order.recalculate
-    self.amount = self.order.total
-    self.save(validate: false)
-  end
-
-  ::Spree::Payment.prepend self
 end
