14a402e4e09d8332f061cd2930b3a5726f732680
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-08-01 14:11:44 -0700

    :recycle: Refactor CustomCashMethod class definition
    - Fixes `PaymentMethod is not a module`

--- app/models/spree/payment_method/custom_cash_method.rb
+++ app/models/spree/payment_method/custom_cash_method.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-module Spree
-    module PaymentMethod
-        class CustomCashMethod < Spree::PaymentMethod::Check
-            has_many :payment_promotion_rules, foreign_key: :payment_method_id, dependent: :destroy, inverse_of: :payment_method
-            has_many :purchase_orders, class_name: "Spree::PurchaseOrder", dependent: :destroy, inverse_of: :payment_method
-            # belongs_to :orders, foreign_key: :order_id
+class Spree::PaymentMethod::CustomCashMethod < Spree::PaymentMethod::Check
+    has_many :payment_promotion_rules, foreign_key: :payment_method_id, dependent: :destroy, inverse_of: :payment_method
+    has_many :purchase_orders, class_name: "Spree::PurchaseOrder", dependent: :destroy, inverse_of: :payment_method
+    # belongs_to :orders, foreign_key: :order_id
 
-            def payment_source_class
-                Spree::PurchaseOrder
-            end
+    def payment_source_class
+        Spree::PurchaseOrder
+    end
 
-            def auto_capture
-                false
-            end
-        end
+    def auto_capture
+        false
     end
 end
