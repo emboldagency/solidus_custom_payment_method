aa9a423b7ff721e79e069dcc16177b2be3f48924
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-08-04 08:43:08 -0700

    :bug: Fix deface to follow zeitwerk autoload convention

--- app/overrides/admin_display_po_num.rb
+++ app/overrides/admin_display_po_num.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-Deface::Override.new(
-    virtual_path: "spree/admin/payments/_list",
-    name: "admin_payments_list_remove_link",
-    replace_contents: "[data-hook=payments_row] td:nth-child(1)",
-    partial: "spree/admin/payments/_list/payment_number",
-)
+module AdminDisplayPoNum
+    Deface::Override.new(
+        virtual_path: "spree/admin/payments/_list",
+        name: "admin_payments_list_remove_link",
+        replace_contents: "[data-hook=payments_row] td:nth-child(1)",
+        partial: "spree/admin/payments/_list/payment_number",
+    )
 
-Deface::Override.new(
-    virtual_path: "spree/admin/payments/_list",
-    name: "admin_payments_list_add_po_num",
-    replace_contents: "[data-hook=payments_row] td:nth-child(3)",
-    partial: "spree/admin/payments/_list/payment_method",
-)
+    Deface::Override.new(
+        virtual_path: "spree/admin/payments/_list",
+        name: "admin_payments_list_add_po_num",
+        replace_contents: "[data-hook=payments_row] td:nth-child(3)",
+        partial: "spree/admin/payments/_list/payment_method",
+    )
+end
